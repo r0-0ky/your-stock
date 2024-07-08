e16f4899df935747bd6e8b3742c91747096ecbe1
Max <maxhard243@gmail.com>
2024-07-08 19:45:09 +0300

    first commit

--- src/app/stores/tg-data-store/tg-data.store.ts
+++ src/app/stores/tg-data-store/tg-data.store.ts
@@ -4,7 +4,7 @@ import { TgData } from './tg-data.types'
 export const useTgDataStore = create((set) => ({
   tg: null,
   userId: null,
-  setTg: (tg: TgData) => set(() => ({
+  setTg: (tg: any) => set(() => ({
     tg: tg,
     userId: tg?.initDataUnsafe?.user?.id
   })),
